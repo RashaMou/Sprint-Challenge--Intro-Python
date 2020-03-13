8e737934a7c5ec9282e37dd4ef35bfa1a6bffae0
Rasha Moumneh <rasha@fastmail.com>
2020-03-13 13:44:00 -0400

    Fixed float issue for cityreader

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -32,7 +32,7 @@ def cityreader(cities=[]):
     csv_reader = csv.reader(csv_file)
     next(csv_reader)
     for row in csv_reader:
-      cities.append(City(row[0], row[3], row[4]))
+      cities.append(City(row[0], float(row[3]), float(row[4])))
     return cities
 
 cityreader(cities)
